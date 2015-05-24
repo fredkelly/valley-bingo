6f52e92b43a44305cffda142a8a7f257f920c40a
Freddy Kelly <me@fredkelly.net>
2015-05-23 19:18:23 -0700

    highlight completed rows/columns

--- shuffle.js
+++ shuffle.js
@@ -1,13 +1,42 @@
 $(function() {
+  var GRID_SIZE = 4;
   var $table = $('table');
   var $cells = $('td', $table).sort(function() { return 0.5 - Math.random() });
 
   $cells
     .each(function(i, c) {
-      $('tr:eq(' + Math.floor(i/4) + ')').append(c);
+      $('tr:eq(' + Math.floor(i/GRID_SIZE) + ')').append(c);
     })
-    .on('click', function(e) {
+    .one('click', function(e) {
       $(e.target).addClass('called');
+      $table.trigger('call', $cells.index(e.target));
     })
     .css('cursor', 'pointer');
+
+  var totals = {
+    rows: {},
+    columns: {}
+  };
+
+  var findWinners = function(totals) {
+    return Object.keys(totals).filter(function(t) {
+      return totals[t] == GRID_SIZE;
+    }).map(Number);
+  };
+
+  $table.on('call', function(e, cell) {
+    var column = cell % GRID_SIZE,
+        row = cell / GRID_SIZE | 0;
+
+    totals.rows[row] = (totals.rows[row] || 0) + 1;
+    totals.columns[column] = (totals.columns[column] || 0) + 1;
+
+    findWinners(totals.rows).map(function(r) {
+      $('tr:nth-of-type(' + (r + 1) + ') > td', $table).addClass('winner');
+    });
+
+    findWinners(totals.columns).map(function(c) {
+      $('tr > td:nth-of-type(' + (c + 1) + ')', $table).addClass('winner');
+    });
+  });
 });
